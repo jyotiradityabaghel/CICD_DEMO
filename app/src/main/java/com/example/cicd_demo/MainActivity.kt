81af85637e819fea2b307569d5d62db0348ed9ba
rocke <rocketbasubaghel@gmail.com>
2023-03-02 19:26:19 +0530

    Second Commit

--- app/src/main/java/com/example/cicd_demo/MainActivity.kt
+++ app/src/main/java/com/example/cicd_demo/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cicd_demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "1541ce6b-c9dd-40d9-93b6-b77d6847a153",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
